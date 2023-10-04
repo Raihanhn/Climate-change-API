27af18421189ef9bf28e350b5a336fee64d5e8af
Raihan <raihanhn20@gmail.com>
2023-10-05 02:48:12 +0600

    api

---
+++ index.js
@@ -0,0 +1,136 @@
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment",
+    base: "",
+  },
+  {
+    name: "dw",
+    address: "https://www.dw.com/en/climate/s-59752983",
+    base: "https://www.dw.com",
+  },
+  {
+    name: "theguardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "thedailystar",
+    address: "https://www.thedailystar.net/environment",
+    base: "",
+  },
+  {
+    name: "cityam",
+    address:
+      "https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/",
+    base: "",
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/international/section/climate",
+    base: "",
+  },
+  {
+    name: "latimes",
+    address: "https://www.latimes.com/environment",
+    base: "",
+  },
+  {
+    name: "smh",
+    address: "https://www.smh.com.au/environment/climate-change",
+    base: "https://www.smh.com.au",
+  },
+  {
+    name: "bbc",
+    address: "https://www.bbc.co.uk/news/science_and_environment",
+    base: "https://www.bbc.co.uk",
+  },
+  {
+    name: "es",
+    address: "https://www.standard.co.uk/topic/climate-change",
+    base: "https://www.standard.co.uk",
+  },
+  {
+    name: "sun",
+    address: "https://www.thesun.co.uk/topic/climate-change-environment/",
+    base: "",
+  },
+  {
+    name: "dm",
+    address:
+      "https://www.dailymail.co.uk/news/climate_change_global_warming/index.html",
+    base: "",
+  },
+  {
+    name: "nyp",
+    address: "https://nypost.com/tag/climate-change/",
+    base: "",
+  },
+];
+const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json(
+    "Welcome to my Climate change news API. You can view json data -> http://localhost:8000/news or http://localhost:8000/news/dw (Ex: dm, nyp, dw, sun, es, bbc, smh, latimes, nyt, cityam, thedailystar, thetimes, theguardian ) "
+  );
+});
+
+app.get("/news", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newspaperAdress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
+  axios
+    .get(newspaperAdress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticles.push({
+          title,
+          url: newspaperBase + url,
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`server runing on PORT ${PORT} `));
